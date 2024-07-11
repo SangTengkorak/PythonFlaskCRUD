101475ba288013762101ca975137844e8519d43c
mastengkorak <mypratomo@gmail.com>
2024-07-11 12:53:21 +0700

    Add requirements.txt

--- init_db.py
+++ init_db.py
@@ -1,24 +1,22 @@
 import psycopg2 
 from config import load_config
 
-app = Flask(__name__)
 config = load_config()
-  
-# Connect to the database 
-conn =  psycopg2.connect(**config)
-cur = conn.cursor()
 
-# if you already have any table or not id doesnt matter this  
-# will create a products table for you. 
-cur.execute( 
-    '''CREATE TABLE IF NOT EXISTS todos (
-    id SERIAL PRIMARY KEY, 
-    item TEXT NOT NULL, 
-    completed BOOLEAN DEFAULT FALSE);''')
+def create_table():
+    command = '''CREATE TABLE IF NOT EXISTS todos (
+                            id SERIAL PRIMARY KEY, 
+                            item TEXT NOT NULL, 
+                            completed BOOLEAN DEFAULT FALSE);'''
 
-# Commit the changes
-conn.commit()
+    try:
+        config = load_config()
+        with psycopg2.connect(**config) as conn:
+            with conn.cursor() as cur:
+                # execute the CREATE TABLE statement
+                    cur.execute(command)
+    except (psycopg2.DatabaseError, Exception) as error:
+        print(error)
 
-# Close the cursor and connection
-cur.close()
-conn.close()
+if __name__ == '__main__':
+    create_table()
